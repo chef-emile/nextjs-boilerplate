af35f9b1064811dd89bde3352ace732dcaeb846f
chef-emile <emile.wallart@gmail.com>
2026-07-03 18:58:10 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,21 @@
 import { supabase } from "../lib/supabase";
 import Link from "next/link";
 
-
 export default async function Home() {
   const { data: recettes } = await supabase
     .from("recettes")
     .select("*");
 
   return (
-<Link
-  href="/ingredients"
-  className="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg"
->
-  Explorer les ingrédients
-</Link>
-    
     <main>
       <h1>Mes recettes</h1>
+
+      <Link
+        href="/ingredients"
+        className="inline-block mt-4 mb-4 bg-blue-500 text-white px-4 py-2 rounded-lg"
+      >
+        Explorer les ingrédients
+      </Link>
 
       <ul>
         {recettes?.map((recette) => (
@@ -26,8 +25,5 @@ export default async function Home() {
         ))}
       </ul>
     </main>
-
-
-    
   );
 }
