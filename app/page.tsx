344cecab6809e60b8b88203adf6227d7f09af9ab
chef-emile <emile.wallart@gmail.com>
2026-07-06 10:56:51 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,19 @@ export default async function Home() {
     <main>
       <h1>Mes recettes</h1>
 
-      <Link
-        href="/ingredients"
-        className="inline-block mt-4 mb-4 bg-blue-500 text-white px-4 py-2 rounded-lg"
-      >
-        Explorer les ingrédients
-      </Link>
+      <div className="flex gap-4 my-4">
+        /ingredients
+          Explorer les ingrédients
+        </Link>
+
+        /ajouter-ingredient
+          Ajouter un ingrédient
+        </Link>
+
+        /ajouter-recette
+          Ajouter une recette
+        </Link>
+      </div>
 
       <ul>
         {recettes?.map((recette) => (
