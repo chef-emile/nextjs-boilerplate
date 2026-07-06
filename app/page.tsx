13d3101b3ca871a7e95d1282ed2da94854b1ba21
chef-emile <emile.wallart@gmail.com>
2026-07-06 12:11:41 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,6 @@ export default async function Home() {
   <Link
     href="/ingredients"
     className="bg-blue-500 text-white px-4 py-2 rounded"
- ="bg-green-500 text-white px-4 py-2 rounded"
   >
     Ajouter un ingrédient
   </Link>
