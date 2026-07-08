ca5b207864eb752a024e8b71d2e885c17bb69427
chef-emile <emile.wallart@gmail.com>
2026-07-08 20:03:44 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import { supabase } from "../lib/supabase";
 import Link from "next/link";
 
+import { supabase } from "../lib/supabase";
+
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const { data: recettes } = await supabase
     .from("recettes")
