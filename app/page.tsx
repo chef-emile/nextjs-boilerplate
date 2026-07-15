80bd5b8d73c22fdc6d602d330ea77a4b6397b242
chef-emile <emile.wallart@gmail.com>
2026-07-15 17:34:28 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,15 @@
 import { supabase } from "../lib/supabase";
 import Link from "next/link";
 export const dynamic = 'force-dynamic'
-
 export default async function Home() {
   const { data: recettes } = await supabase
     .from("recettes")
     .select("*");
-
   return (
-
     <main className="p-6">
       <h1 className="text-2xl font-bold mb-4">
         Mes recettes
       </h1>
-
       <div className="flex gap-4 mb-6 flex-wrap">
         <Link
           href="/ingredients"
@@ -21,38 +17,34 @@ export default async function Home() {
         >
           Explorer les ingrédients
         </Link>
-
          <Link
           href="/ajouter-ingredient"
           className="bg-blue-500 text-white px-4 py-2 rounded"
           >
           Ajouter un ingrédient      
         </Link>
-
         <Link
           href="/ajouter-recette"
           className="bg-blue-500 text-white px-4 py-2 rounded"
           >
           Ajouter une recette      
          </Link>
-
         
       </div>
       
       <ul>
         {recettes?.map((recette) => (
-         <li key={recette.recette_id}>
-          <Link href={`/recettes/${recette.recette_id}`} className="text-blue-500 hover:underline">
-            {recette.nom}
-          </Link>
-        </li>
+          <li key={recette.recette_id} className="mb-1">
+            <Link
+              href={`/recettes/${recette.recette_id}`}
+              className="text-blue-600 hover:underline"
+            >
+              {recette.nom}
+            </Link>
+          </li>
         ))}
       </ul>
  
-
-
     </main>
   );
 }
-
-
