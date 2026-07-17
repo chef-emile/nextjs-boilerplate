0cd35078ef350e8451546add769ddeacc550a289
chef-emile <emile.wallart@gmail.com>
2026-07-17 11:31:36 +0200

    Update page.tsx

--- app/import/page.tsx
+++ app/import/page.tsx
@@ -2,15 +2,16 @@
 
 import ImportIngredients from './ImportIngredients'
 import ImportRecettes from './ImportRecettes'
+import ExportEtReset from './ExportEtReset'
 
 export default function ImportPage() {
   return (
     <main className="p-6">
       <h1 className="text-2xl font-bold mb-6">Import en masse</h1>
-
       <div className="flex flex-col gap-6 max-w-2xl">
         <ImportIngredients />
         <ImportRecettes />
+        <ExportEtReset />
       </div>
     </main>
   )
